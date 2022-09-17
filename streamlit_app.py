3bdd684aa4a397a9ffb19b43a5ca10ef09402b89
snowtraining2022 <113749488+snowtraining2022@users.noreply.github.com>
2022-09-17 00:05:51 -0500

    Update streamlit_app.py
    Added  a new  breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+steamit.txtt('Jard-Boiled Free-Range Egg')
